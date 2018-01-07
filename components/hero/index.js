c01fdb7473e33a67a2ecb8e1dd11ebbb71bb60a3
Felipe blini <Felipe.blini@gmail.com>
2018-01-07 16:53:04 -0300

    migrado parallax para o materialize, importando bootstrap e materialize por modulos, removido animate.css, refatorado codigo

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,11 +1,26 @@
 import './index.scss';
 import './icons.svg';
 
-let $body = document.body;
+const $body = document.body;
 
-let toggleFixedNavbarPosition = function () {
-  let currentScroll = Math.round(document.documentElement.scrollTop);
-  let introHeight = document.querySelectorAll('.intro')[0].offsetHeight;
+const toggleFixedNavbar = function () {
+  const currentScroll = Math.round(document.documentElement.scrollTop);
+  const introHeight = document.querySelectorAll('.intro')[0].offsetHeight;
+
+  if (currentScroll > introHeight - 80) {
+    // fix navbar
+    $body.classList.add('fixed-navbar');
+  }
+
+  if (currentScroll < introHeight - 80) {
+    // unfix navbar
+    $body.classList.remove('fixed-navbar');
+  }
+};
+
+const toggleFixedHeader = function() {
+  const currentScroll = Math.round(document.documentElement.scrollTop);
+  const introHeight = document.querySelectorAll('.intro')[0].offsetHeight;
 
   if (currentScroll > 10) {
     // fix header
@@ -21,27 +36,22 @@ let toggleFixedNavbarPosition = function () {
     // unfix header
     $body.classList.remove('fixed-header');
   }
-
-  if (currentScroll > introHeight - 80) {
-    // fix navbar
-    $body.classList.add('fixed-navbar');
-  }
-
-  if (currentScroll < introHeight - 80) {
-    // unfix navbar
-    $body.classList.remove('fixed-navbar');
-  }
 }
 
+const adjustHeroContentOpacity = function() {
+  const newOpacity = (230 - $(window).scrollTop()) / 100;
+  document.querySelector('#hero-content').style.opacity = newOpacity;
+};
 
-$(window).scroll(function () {
-  $('#hero-content').css({ 'opacity': (230 - $(window).scrollTop()) / 100 });
-  toggleFixedNavbarPosition();
-});
-
-function adjustHeroContentSize() {
-  document.getElementById('hero-content-fadein-scroll').style.width = (window.innerWidth - 110) + 'px';
+const adjustHeroContentSize = function () {
+  document.querySelector('#hero-content').style.width = (window.innerWidth - 110) + 'px';
 }
+
+window.onscroll = function () {
+  adjustHeroContentOpacity();
+  toggleFixedNavbar();
+  toggleFixedHeader();
+};
 
 window.onresize = (function () {
   adjustHeroContentSize();
@@ -60,6 +70,8 @@ sr.reveal('.reveal-left', { origin: 'left' });
 sr.reveal('.reveal-right', { origin: 'right' });
 sr.reveal('.reveal-bottom', { origin: 'bottom' });
 sr.reveal('.reveal-duration-800', { duration: 800 });
+sr.reveal('.reveal-no-opacity', { opacity: 1 });
+sr.reveal('.reveal-no-scale', { scale: 1 });
 
 sr.reveal('.content-header-with-options .btn-group.filter', {
   delay: 800,
@@ -68,19 +80,10 @@ sr.reveal('.content-header-with-options .btn-group.filter', {
 });
 sr.reveal('.mbr-gallery-item.reveal-sequence', 50);
 sr.reveal('.material-item.reveal-sequence', 50);
-sr.reveal('.footer .reveal-sequence', 50);
 sr.reveal('.sponsor .reveal-sequence', 50);
-// sr.reveal('.reveal-no-opacity', { opacity: 1 });
-// sr.reveal('.reveal-no-scale', { scale: 1 });
+sr.reveal('.footer .reveal-sequence', 15);
 
-$(document).ready(() => {
-  $('.content-box').parallax({
-    imageSrc: './components/hero/vista-da-catedral-praa-9-de-julho-presidente-prudente-sp-2000x1500.jpg',
-    naturalHeight: '100vh'
-  });
+// Materialize Parallax
+$(document).ready(function(){
+  $('.parallax').parallax();
 });
-
-// var jarallax = new Jarallax()
-// jarallax.addAnimation('.jarallax_background',
-//   [{ progress: '0%', top: '0' },
-//   { progress: '100%', top: '-500px' }])
