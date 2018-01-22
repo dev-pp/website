1e7f9afc3bc7f6ba534569e2b26c626800f08bc4
Felipe blini <Felipe.blini@gmail.com>
2018-01-21 22:19:30 -0300

    lista de material arrumada close #3

--- src/components/newsletters/DevppNewsletters.vue
+++ src/components/newsletters/DevppNewsletters.vue
@@ -1,45 +1,32 @@
 <template>
   <section id="newsletters" class="newsletters reveal">
-    <!-- <div class="container">
-          <h3>RECEBA AS NOVIDADES</h3>
-          <p>
-            Receba uma newsletter semanal sobre
-            <strong>novas tecnologias, front-end, back-end, inovações, notícias, oportunidades de trabalho, novos meetups da Dev-pp e também eventos em Presidente Prudente e região e no Brasil todo</strong>.
-          </p>
-          <form action="https://felipeblini.us12.list-manage.com/subscribe/post-json?u=aa251961375a2fd4ac20a6132&amp;id=5718990c28&c=?" method="get" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form" novalidate>
-            <div>
-              <input value="" name="EMAIL" type="text" class="form-control input-lg" placeholder="Digite o seu endereço de e-mail...">
-            </div>
-            <div>
-              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_aa251961375a2fd4ac20a6132_5718990c28" tabindex="-1" value=""></div>
-              <div class="clear"><input type="submit" value="INSCREVER" name="subscribe" id="mc-embedded-subscribe" class="btn btn-lg btn-custom"></div>
-            </div>
-          </form>
-        </div> -->
-
     <div class="container">
       <h3>RECEBA AS NOVIDADES</h3>
       <p>
         Receba uma newsletter semanal sobre
         <strong>novas tecnologias, front-end, back-end, inovações, notícias, oportunidades de trabalho, novos meetups da Dev-pp e também eventos em Presidente Prudente e região e no Brasil todo</strong>.
       </p>
-      <div class="messages">
-        <div class="alert alert-danger" v-if="errorMessage && !successMessage" transition="fade"><b>Erro:</b> {{ errorMessage }}</div>
-        <div class="alert alert-success" v-if="successMessage" transition="fade"><b>Tudo certo:</b> {{ successMessage }}</div>
-      </div>
-      <form class="form" v-if="!successMessage" @submit.prevent="subscribe($event)">
-        <div>
-          <input placeholder="Digite o seu endereço de e-mail..." v-model="email" name="EMAIL" type="text" class="form-control input-lg" id="email" />
+      <div class="form-container">
+        <div class="messages">
+          <div class="alert alert-danger" v-if="errorMessage && !successMessage" transition="fade">
+            <b>Erro:</b> {{ errorMessage }}</div>
+          <div class="alert alert-success" v-if="successMessage" transition="fade">
+            <b>Tudo certo:</b> {{ successMessage }}</div>
         </div>
-        <div>
-          <!-- Not to be filled up by humans-->
-          <div style="position: absolute; left: -5000px;" aria-hidden="true">
-            <input type="text" name="b_aa251961375a2fd4ac20a6132_5718990c28" tabindex="-1" value="">
+        <form class="form" v-if="!successMessage" @submit.prevent="subscribe($event)">
+          <div>
+            <input placeholder="Digite o seu endereço de e-mail..." v-model="email" name="EMAIL" type="text" class="form-control input-lg" id="email" />
           </div>
+          <div>
+            <!-- Not to be filled up by humans-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+              <input type="text" name="b_aa251961375a2fd4ac20a6132_5718990c28" tabindex="-1" value="">
+            </div>
 
-          <input type="submit" value="INSCREVER" name="subscribe" class="btn btn-lg btn-custom">
-        </div>
-      </form>
+            <input type="submit" value="INSCREVER" name="subscribe" class="btn btn-lg btn-custom">
+          </div>
+        </form>
+      </div>
     </div>
   </section>
 </template>
@@ -70,23 +57,23 @@ export default {
   },
   methods: {
     subscribe: function(e) {
-      //const url = 'https://felipeblini.us12.list-manage.com/subscribe/post-json?u=aa251961375a2fd4ac20a6132&id=5718990c28&EMAIL=teste@postman.get';
       jsonp(this.preparedAction, { param: 'c' }, (err, data) => {
         if (data && data.result === 'success') {
           this.successMessage = 'Email cadastrado, por favor acesse o seu email e confirme sua inscrição.';
         } else {
-          const _self = this;
-          setTimeout(function() {
-            _self.errorMessage = null;
-            console.log('object');
-          }, 4000);
-          if(data && data.msg.indexOf('already subscribed') > -1) {
+          if (data && data.msg.indexOf('already subscribed') > -1) {
             this.errorMessage = 'Este e-mail já está inscrito.';
-          } else if(data && data.msg.indexOf('@') > -1 || data.msg.indexOf('enter a value'))  {
+          } else if (data && data.msg.indexOf('@') > -1 || data.msg.indexOf('enter a value')) {
             this.errorMessage = 'Por favor digite um e-mail válido';
           } else {
             this.errorMessage = 'Ocorreu um erro, por favor tente novamente.';
           }
+
+          // const _self = this;
+          // setTimeout(function() {
+          //   _self.errorMessage = null;
+          //   console.log('object');
+          // }, 4000);
 
           if (err) {
             console.log('err', err);
