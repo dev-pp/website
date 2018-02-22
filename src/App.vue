716a7e9c1a124eabf7db529809dfd2094b50fea6
Felipe blini <Felipe.blini@gmail.com>
2018-02-22 00:47:11 -0300

    criado componente material-item, criado servico buscando materiais do firebase e listando materiais dinamicamente no template do comónente material

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <devpp-join-us class="dark-bg" />
     <devpp-meetups />
     <devpp-gallery />
-    <devpp-materials />
+    <devpp-material-list />
     <devpp-palestre />
     <devpp-weekly form-action="https://tech.us17.list-manage.com/subscribe/post?u=dd95564dc86cde20bf6712519&amp;id=69e305d9aa" />
     <devpp-sponsors />
@@ -25,7 +25,7 @@ import DevppMembers from './components/members/DevppMembers.vue';
 import DevppJoinUs from './components/join_us/DevppJoinUs.vue';
 import DevppMeetups from './components/meetups/DevppMeetups.vue';
 import DevppGallery from './components/gallery/DevppGallery.vue';
-import DevppMaterials from './components/material/DevppMaterial.vue';
+import DevppMaterialList from './components/material/DevppMaterialList.vue';
 import DevppPalestre from './components/palestre/DevppPalestre.vue';
 import DevppWeekly from './components/weekly/DevppWeekly.vue';
 import DevppSponsors from './components/sponsors/DevppSponsors.vue';
@@ -42,7 +42,7 @@ export default {
     DevppJoinUs,
     DevppMeetups,
     DevppGallery,
-    DevppMaterials,
+    DevppMaterialList,
     DevppPalestre,
     DevppWeekly,
     DevppSponsors,
---
+++ src/components/material/DevppMaterialItem.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="material-wrapper-item">
+    <div class="material-item content-panel reveal-sequence">
+      <div class="palestrante">
+        <div class="overflow"></div>
+        <div class="foto">
+          <img :src="palestrante.foto" alt="" class="pull-left">
+        </div>
+        <div class="info">
+          <div>
+            <mic-icon class="icon" />
+            <span>{{ palestrante.nome }}</span>
+          </div>
+          <div>
+            <calendar-icon class="icon" />
+            <span>{{ palestra.data }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="caption">
+        <h4>{{ palestra.titulo }}</h4>
+        <p>{{ palestra.desc }}</p>
+        <p>
+          <a :href="recursos.slides" :disabled="!recursos.slides" class="btn btn-default" role="button" target="_blank">
+            <presentation-icon class="icon" /> Slides
+          </a>
+          <a :href="recursos.codigofonte" :disabled="!recursos.codigofonte" class="btn btn-default" role="button" target="_blank">
+            <github-icon class="icon" /> Fonte
+          </a>
+          <a :href="recursos.video" :disabled="!recursos.video" class="btn btn-default" role="button" target="_blank">
+            <youtube-icon class="icon youtube" /> Vídeo
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CalendarIcon from "./material--icons.svg?icon-calendar";
+import MicIcon from "./material--icons.svg?icon-mic";
+import GithubIcon from "./material--icons.svg?icon-github";
+import PresentationIcon from "./material--icons.svg?icon-presentation";
+import YoutubeIcon from "./material--icons.svg?icon-youtube";
+
+export default {
+  name: 'devpp-material-item',
+  props: {
+    palestrante: '',
+    palestra: '',
+    recursos: ''
+  },
+  components: {
+    CalendarIcon,
+    MicIcon,
+    GithubIcon,
+    PresentationIcon,
+    YoutubeIcon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './material-item--icons.scss';
+
+.material-wrapper-item {
+  display: inline-block;
+  margin-bottom: .5rem;
+  padding: 0.3rem;
+  width: 100%;
+  background: #efefef;
+
+  .material-item {
+    .palestrante {
+      display: flex;
+      overflow: hidden;
+      padding: 10px;
+
+      .overflow {
+        width: 47px;
+        position: absolute;
+        height: 80px;
+        left: 19px;
+        background: #fff;
+        z-index: 2;
+        @media(max-width: 305px) {
+          display: none;
+        }
+      }
+
+      img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        border: solid 3px white;
+        position: relative;
+        z-index: 2;
+        @media(max-width: 305px) {
+          display: none;
+        }
+      }
+
+      .info {
+        padding: 8px 0 8px 70px;
+        flex: 2;
+        margin-left: -60px;
+        position: relative;
+        z-index: 1;
+        font-size: 14px;
+        height: 70px;
+        top: 7px;
+
+        div {
+          span {
+            position: relative;
+            top: -2px;
+            left: 4px;
+          }
+        }
+        @media(max-width: 305px) {
+          padding: 8px 8px 8px 16px;
+          font-size: 12px;
+          height: 40px;
+          border-top-left-radius: 25px;
+          border-bottom-left-radius: 25px;
+          margin-left: 0;
+
+          img {
+            display: none;
+          }
+
+          div {
+            float: left;
+
+            span {
+              position: inherit;
+              top: 0;
+              left: 0;
+            }
+          }
+
+          div:nth-child(1) {
+            margin-right: 3px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+
