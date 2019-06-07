7a597dd82e07d20d53518b315d0f3446f1552a3e
Felipe blini <Felipe.blini@gmail.com>
2019-06-07 16:08:51 -0300

    changing sponsors, links and navbar menu

--- src/components/navbar/DevppNavbar.vue
+++ src/components/navbar/DevppNavbar.vue
@@ -1,38 +1,53 @@
 <template>
-  <section id="navbar" class="custom-navbar">
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-          </div>
-          <div class="collapse navbar-collapse custom-navbar-nav" id="myNavbar">
-            <ul class="nav navbar-nav">
-              <li><a href="#about">Sobre</a></li>
-              <li><a href="#members">Membros</a></li>
-              <li><a href="#meetups">Meetups</a></li>
-              <li><a href="#photos">Fotos</a></li>
-              <li><a href="#material">Material</a></li>
-              <li><a href="#palestre">Palestre</a></li>
-              <li><a href="#contact">Contato</a></li>
-            </ul>
-            <a class="btn btn-md btn-custom pull-right" href="https://meetup.com/dev-pp" target="_blank">JUNTE-SE A NÓS</a>
-          </div>
+  <section
+    id="navbar"
+    class="custom-navbar"
+  >
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button
+            type="button"
+            class="navbar-toggle"
+            data-toggle="collapse"
+            data-target="#myNavbar"
+          >
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
         </div>
-      </nav>
-    </section>
+        <div
+          class="collapse navbar-collapse custom-navbar-nav"
+          id="myNavbar"
+        >
+          <ul class="nav navbar-nav">
+            <li><a href="#about">Sobre</a></li>
+            <li><a href="#members">Membros</a></li>
+            <li><a href="#meetups">Meetups</a></li>
+            <li><a href="#photos">Fotos</a></li>
+            <li><a href="#material">Material</a></li>
+            <li><a href="#palestre">Palestre</a></li>
+            <li><a href="#sponsors">Parceiros</a></li>
+            <li><a href="#contact">Contato</a></li>
+          </ul>
+          <a
+            class="btn btn-md btn-custom pull-right"
+            href="https://meetup.com/dev-pp"
+            target="_blank"
+          >JUNTE-SE A NÓS</a>
+        </div>
+      </div>
+    </nav>
+  </section>
 </template>
 
 <script>
-
 export default {
-  name: 'devpp-navbar',
-}
+  name: "devpp-navbar"
+};
 </script>
 
 <style lang="scss" scoped>
-@import './navbar--style.scss';
+@import "./navbar--style.scss";
 </style>
--- src/components/weekly/DevppWeekly.vue
+++ src/components/weekly/DevppWeekly.vue
@@ -1,31 +1,76 @@
 <template>
-  <section id="weekly" class="weekly reveal">
+  <section
+    id="weekly"
+    class="weekly reveal"
+  >
     <div class="container">
-      <h3>DEV-PP Weekly</h3>
-      <p>
-        Receba uma newsletter semanal sobre
-        <strong>novas tecnologias, front-end, back-end, inovações, notícias, oportunidades de trabalho, novos meetups da Dev PP e também eventos em Presidente Prudente e região e no Brasil todo</strong>.
+      <h3>DEV-PP Newsletter</h3>
+      <p style="font-size: 20px">
+        Receba uma vez por mês nosso email com conteúdo curados referentes
+        a
+        <strong>novidades do mercado de TI, desenvolvimento
+          front-end e back-end, user experience, inovações, oportunidades de
+          trabalho e o que mais der na teia 🖖. </strong>Além disso você fica
+        ligado(a) quando acontece os
+        <strong>meetups do Dev PP</strong>, do <sttrong>GDG Prudente</sttrong>
+        e também <strong>eventos techs em Presidente Prudente
+          e região e os melhores em todo o Brasil</strong>.
       </p>
       <div class="form-container">
         <div class="messages">
-          <div class="alert alert-danger" v-if="errorMessage && !successMessage" transition="fade">
+          <div
+            class="alert alert-danger"
+            v-if="errorMessage && !successMessage"
+            transition="fade"
+          >
             <b>Erro:</b> {{ errorMessage }}</div>
-          <div class="alert alert-success" v-if="successMessage" transition="fade">
+          <div
+            class="alert alert-success"
+            v-if="successMessage"
+            transition="fade"
+          >
             <b>Tudo certo:</b> {{ successMessage }}
-            <a href="#" @click.prevent.stop="onSuccessBack">Voltar</a>
+            <a
+              href="#"
+              @click.prevent.stop="onSuccessBack"
+            >Voltar</a>
           </div>
         </div>
-        <form class="form" v-if="!successMessage" @submit.prevent="subscribe($event)">
+        <form
+          class="form"
+          v-if="!successMessage"
+          @submit.prevent="subscribe($event)"
+        >
           <div>
-            <input placeholder="Digite o seu endereço de e-mail..." v-model="email" name="EMAIL" type="text" class="form-control input-lg" id="email" />
+            <input
+              placeholder="Digite o seu endereço de e-mail..."
+              v-model="email"
+              name="EMAIL"
+              type="text"
+              class="form-control input-lg"
+              id="email"
+            />
           </div>
           <div>
             <!-- Not to be filled up by humans-->
-            <div style="position: absolute; left: -5000px;" aria-hidden="true">
-              <input type="text" name="b_aa251961375a2fd4ac20a6132_5718990c28" tabindex="-1" value="">
+            <div
+              style="position: absolute; left: -5000px;"
+              aria-hidden="true"
+            >
+              <input
+                type="text"
+                name="b_aa251961375a2fd4ac20a6132_5718990c28"
+                tabindex="-1"
+                value=""
+              >
             </div>
 
-            <input type="submit" value="INSCREVER" name="subscribe" class="btn btn-lg btn-custom">
+            <input
+              type="submit"
+              value="INSCREVER"
+              name="subscribe"
+              class="btn btn-lg btn-custom"
+            >
           </div>
         </form>
       </div>
@@ -34,10 +79,10 @@
 </template>
 
 <script>
-import jsonp from 'jsonp';
+import jsonp from "jsonp";
 
 export default {
-  name: 'devpp-weekly',
+  name: "devpp-weekly",
   props: {
     formAction: {
       required: true,
@@ -46,7 +91,7 @@ export default {
   },
   data: function() {
     return {
-      email: '',
+      email: "",
       response: {},
       errorMessage: null,
       successMessage: null
@@ -54,31 +99,37 @@ export default {
   },
   computed: {
     preparedAction: function() {
-      return this.formAction.replace('/post?', '/post-json?').concat(`&EMAIL=${this.email}`)
+      return this.formAction
+        .replace("/post?", "/post-json?")
+        .concat(`&EMAIL=${this.email}`);
     }
   },
   methods: {
     subscribe: function(e) {
-      jsonp(this.preparedAction, { param: 'c' }, (err, data) => {
-        if (data && data.result === 'success') {
-          this.successMessage = 'Email cadastrado, por favor acesse o seu email e confirme sua inscrição.';
+      jsonp(this.preparedAction, { param: "c" }, (err, data) => {
+        if (data && data.result === "success") {
+          this.successMessage =
+            "Email cadastrado, por favor acesse o seu email e confirme sua inscrição.";
         } else {
-          if (data && data.msg.indexOf('already subscribed') > -1) {
-            this.errorMessage = 'Este e-mail já está inscrito.';
-          } else if (data && data.msg.indexOf('@') > -1 || data.msg.indexOf('enter a value')) {
-            this.errorMessage = 'Por favor digite um e-mail válido';
+          if (data && data.msg.indexOf("already subscribed") > -1) {
+            this.errorMessage = "Este e-mail já está inscrito.";
+          } else if (
+            (data && data.msg.indexOf("@") > -1) ||
+            data.msg.indexOf("enter a value")
+          ) {
+            this.errorMessage = "Por favor digite um e-mail válido";
           } else {
-            this.errorMessage = 'Ocorreu um erro, por favor tente novamente.';
+            this.errorMessage = "Ocorreu um erro, por favor tente novamente.";
           }
 
           const _self = this;
           setTimeout(function() {
             _self.errorMessage = null;
-            console.log('object');
+            console.log("object");
           }, 4000);
 
           if (err) {
-            console.log('err', err);
+            console.log("err", err);
           }
 
           console.log(data);
@@ -86,8 +137,8 @@ export default {
       });
     },
     onSuccessBack(e) {
-      this.successMessage = '';
-      this.email = '';
+      this.successMessage = "";
+      this.email = "";
     }
   }
 };
