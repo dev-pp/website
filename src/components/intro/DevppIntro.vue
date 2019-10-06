e9e79ec4656ab44b817568a6d462b0cd1d43435d
Felipe blini <Felipe.blini@gmail.com>
2019-10-06 13:17:50 -0300

    removed opaciy and reveal

--- src/components/intro/DevppIntro.vue
+++ src/components/intro/DevppIntro.vue
@@ -12,103 +12,96 @@
       <div class="hero-content container">
         <div id="hero-content" class="row">
           <div>
-            <div class="reveal-bottom reveal-distance-70px reveal-duration-800">
-              <h1>DevPP #15 The Conference</h1>
-              <div class="event-date-place">
-                <div class="date">
-                  <img
-                    class="icon"
-                    src="./evento-devpp-the-conference/icones/calendar.png"
-                  />
-                  <span>15 DE OUTUBRO</span>
+            <h1>DevPP #15 The Conference</h1>
+            <div class="event-date-place">
+              <div class="date">
+                <img
+                  class="icon"
+                  src="./evento-devpp-the-conference/icones/calendar.png"
+                />
+                <span>15 DE OUTUBRO</span>
+              </div>
+              <div class="place">
+                <img
+                  class="icon"
+                  src="./evento-devpp-the-conference/icones/map-marker.png"
+                />
+                <span>AUDITÓRIO DO INOVA PRUDENTE</span>
+              </div>
+            </div>
+            <div class="event-info">
+              <div class="event-row row-1">
+                <div class="palestrantes">
+                  <div class="palestrante-destaque">
+                    <div class="foto"></div>
+                    <div class="info">
+                      <h1>Rodrigo PokemaoBR</h1>
+                      <span>
+                        Stand-Up: Arquitetando seus sistemas de forma
+                        (IN)coerente
+                      </span>
+                      <span>
+                        Palestra: Desenvolvendo chatbots mesmo sem saber codar!
+                      </span>
+                    </div>
+                  </div>
+                  <div class="demais-palestrantes">
+                    <h6>Demais palestrantes:</h6>
+                    <div class="lista">
+                      <div class="palestrante">
+                        <div class="foto p1"></div>
+                        <div class="nome">Marcela Maciel</div>
+                      </div>
+                      <div class="palestrante">
+                        <div class="foto p2"></div>
+                        <div class="nome">Leonardo Dias</div>
+                      </div>
+                      <div class="palestrante">
+                        <div class="foto p3"></div>
+                        <div class="nome">Marcelo Santoro</div>
+                      </div>
+                      <a class="ver-agenda in" href="#">VER AGENDA</a>
+                    </div>
+                    <a class="ver-agenda out" href="#">VER AGENDA</a>
+                  </div>
                 </div>
-                <div class="place">
-                  <img
-                    class="icon"
-                    src="./evento-devpp-the-conference/icones/map-marker.png"
-                  />
-                  <span>AUDITÓRIO DO INOVA PRUDENTE</span>
+
+                <div class="buttons">
+                  <div class="apoio">
+                    <h6>APOIO:</h6>
+                    <div class="lista">
+                      <div class="logo foregon" />
+                      <div class="logo forspace" />
+                      <div class="logo cobmais" />
+                      <div class="logo inova-prudente" />
+                      <div class="logo sicoob" />
+                      <div class="logo sw" />
+                    </div>
+                  </div>
+
+                  <div class="btn-inscrevase">
+                    <a href="http://bit.ly/cta-site" class="btn">INSCREVA-SE</a>
+                  </div>
                 </div>
               </div>
-              <div class="event-info">
-                <div class="event-row row-1">
-                  <div class="palestrantes">
-                    <div class="palestrante-destaque">
-                      <div class="foto"></div>
-                      <div class="info">
-                        <h1>Rodrigo PokemaoBR</h1>
-                        <span>
-                          Stand-Up: Arquitetando seus sistemas de forma
-                          (IN)coerente
-                        </span>
-                        <span>
-                          Palestra: Desenvolvendo chatbots mesmo sem saber
-                          codar!
-                        </span>
-                      </div>
-                    </div>
-                    <div class="demais-palestrantes">
-                      <h6>Demais palestrantes:</h6>
-                      <div class="lista">
-                        <div class="palestrante">
-                          <div class="foto p1"></div>
-                          <div class="nome">Marcela Maciel</div>
-                        </div>
-                        <div class="palestrante">
-                          <div class="foto p2"></div>
-                          <div class="nome">Leonardo Dias</div>
-                        </div>
-                        <div class="palestrante">
-                          <div class="foto p3"></div>
-                          <div class="nome">Marcelo Santoro</div>
-                        </div>
-                        <a class="ver-agenda in" href="#">VER AGENDA</a>
-                      </div>
-                      <a class="ver-agenda out" href="#">VER AGENDA</a>
-                    </div>
+              <!-- </row-1 -->
+
+              <div class="event-row row-2">
+                <div class="event-row galera">
+                  <div>
+                    <h6>
+                      A galera que tá fazendo isso acontecer:
+                    </h6>
                   </div>
-
-                  <div class="buttons">
-                    <div class="apoio">
-                      <h6>APOIO:</h6>
-                      <div class="lista">
-                        <div class="logo foregon" />
-                        <div class="logo forspace" />
-                        <div class="logo cobmais" />
-                        <div class="logo inova-prudente" />
-                        <div class="logo sicoob" />
-                        <div class="logo sw" />
-                      </div>
-                    </div>
-
-                    <div class="btn-inscrevase">
-                      <a
-                        href="https://www.meetup.com/dev-pp/events/265419376/"
-                        class="btn"
-                        >INSCREVA-SE</a
-                      >
-                    </div>
-                  </div>
-                </div>
-                <!-- </row-1 -->
-
-                <div class="event-row row-2">
-                  <div class="event-row galera">
-                    <div>
-                      <h6>
-                        A galera que tá fazendo isso acontecer:
-                      </h6>
-                    </div>
-                    <div class="fotos-wrapper">
-                      <div class="foto calistro" />
-                      <div class="foto felipealmeida" />
-                      <div class="foto blini" />
-                      <div class="foto marquini" />
-                      <div class="foto phillipe" />
-                      <div class="foto marcelo" />
-                      <div class="foto wesley" />
-                      <div class="foto carneiro" />
-                    </div>
+                  <div class="fotos-wrapper">
+                    <div class="foto calistro" />
+                    <div class="foto felipealmeida" />
+                    <div class="foto blini" />
+                    <div class="foto marquini" />
+                    <div class="foto phillipe" />
+                    <div class="foto marcelo" />
+                    <div class="foto wesley" />
+                    <div class="foto carneiro" />
                   </div>
                 </div>
               </div>
--- src/components/intro/adjust-hero-layout.js
+++ src/components/intro/adjust-hero-layout.js
@@ -1,13 +1,14 @@
-const adjustHeroContentOpacity = function () {
-  const newOpacity = (230 - $(window).scrollTop()) / 100;
-  document.querySelectorAll('.intro .hero-content')[0].style.opacity = newOpacity;
+const adjustHeroContentOpacity = function() {
+  // const newOpacity = (230 - $(window).scrollTop()) / 100;
+  // document.querySelectorAll('.intro .hero-content')[0].style.opacity = newOpacity;
 };
 
-const adjustHeroContentSize = function () {
-  document.querySelectorAll('.intro .hero-content')[0].style.width = (window.innerWidth - 110) + 'px';
-}
+const adjustHeroContentSize = function() {
+  document.querySelectorAll(".intro .hero-content")[0].style.width =
+    window.innerWidth - 110 + "px";
+};
 
 module.exports = {
   adjustIntroHeroOnScroll: adjustHeroContentOpacity,
-  adjustIntroHeroOnResize: adjustHeroContentSize,
-}
+  adjustIntroHeroOnResize: adjustHeroContentSize
+};
